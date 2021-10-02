4d632606da04ddeac7ed0726e0486424519fe06a
preetha-04 <90676901+preetha-04@users.noreply.github.com>
2021-10-02 21:09:16 +0530

    Add files via upload

---
+++ Task1.java
@@ -0,0 +1,53 @@
+class Task1
+{
+	public static void main(String[] args) 
+	{
+	
+		System.out.println("1. Area of Rectangle with sides 2 and 3");
+		int l=2;
+		int b=3;
+		System.out.println("Area of Rectangle is :"+l*b);
+		System.out.println();
+
+		System.out.println("2.Product of Numbers 8.2 and 6.0");
+		double a=8.2;
+		double c=6.0;
+		System.out.println("product of Numbers is :"+(a*c));
+		System.out.println();
+
+		System.out.println("3. ASCII value of character H");
+		char d='H';
+		int asciivalue=d;
+		System.out.println("ASCII value of character H is :"+asciivalue);
+		System.out.println();
+
+		System.out.println("4. Convert double 100.235 to int");
+		Double e=new Double("100.235");
+		int f=e.intValue();
+		System.out.println("Int value is :"+e);
+		System.out.println();
+
+		System.out.println("5. Add 3 to the ASCII value of the character 'd'");
+		int h=3+'d';
+		System.out.println("ASCII value of the character 'd' is :"+(char)h);
+		System.out.println();
+
+		System.out.println("6. Add an integer variable having value 5 and double variable having value 6.2");
+		int x=5;
+		double y=6.2;
+		System.out.println("value of int and double is :"+x+y);
+		System.out.println();		
+
+		System.out.println("7. Square of the number 3.9");		
+		System.out.println("Square of the number is :"+3.9*3.9);
+	    System.out.println();
+
+		System.out.println("8. Cube of the number 3.2");
+		System.out.println(" Cube of the number is :"+3.2*3.2*3.2);
+        System.out.println();
+
+
+		
+
+	}
+}

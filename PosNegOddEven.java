435f0dc04c70ce71760505f3dd5a65321c9ad336
preetha-04 <90676901+preetha-04@users.noreply.github.com>
2021-10-02 10:13:05 +0530

    Add files via upload

---
+++ PosNegOddEven.java
@@ -0,0 +1,44 @@
+//3. Take 20 integer inputs from user and put them in an array and print the following :
+  //    ---number of positive numbers
+  //	  ---number of negative numbers
+  //	  ---number of odd numbers
+  //	  ---number of even numbers
+  //	  ---number of zeros
+
+import java.util.Scanner;
+
+class PosNegOddEven
+{
+	public static void main(String[] args) 
+	{
+		Scanner s = new Scanner(System.in);
+		int[]z = new int[20];
+		int pos = 0;
+		int neg = 0;
+		int odd = 0;
+		int even = 0;
+		int zero = 0;
+		for(int i=0; i<z.length; i++){
+			System.out.println("Print the value of z["+i+"]");
+			z[i] = s.nextInt();
+
+			if(z[i]>0){
+				pos++;
+			}
+			else if(z[i]<0){
+				neg++;
+			}
+			else{
+				zero++;
+			}
+			if(z[i]%2 == 0){
+				even++;
+			}
+			else{
+				odd++;
+			}
+		}
+		System.out.println("Positive :"+pos+"\nNegative :"+neg+"\nZero :"+zero+"\nodd :"+odd+"\neven :"+even);
+	}
+}
+		

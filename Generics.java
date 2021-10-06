740daa82b3e4a19666708f9cc9803d784770624c
preetha-04 <90676901+preetha-04@users.noreply.github.com>
2021-10-06 21:02:15 +0530

    Add files via upload

---
+++ Generics.java
@@ -0,0 +1,21 @@
+import java.util.ArrayList;
+
+class Generics{
+	public static void main(String[] args) {
+
+		ArrayList<Integer> i = new ArrayList<Integer>();
+
+		i.add(1);
+		i.add(2); 
+		i.add(3);
+		i.add(4);
+		i.add(7);
+		i.add(6);
+		i.add(7);
+		i.add(8);
+		i.add(9);
+		i.add(10);
+
+		System.out.println(i);
+	}
+}

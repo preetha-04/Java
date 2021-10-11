3acaeb0a7e1656c5bfeeb95763b23208fa10d16f
preetha-04 <90676901+preetha-04@users.noreply.github.com>
2021-10-11 20:05:53 +0530

    Add files via upload

---
+++ Tricky.java
@@ -0,0 +1,14 @@
+class rectangle
+{
+	public static void main(String[] args) 
+	{
+		int width=2;
+		int height=3;
+		int area=width*height;
+	
+	
+		System.out.println("Area of rectangle ="+area);
+		}
+
+	}
+}
